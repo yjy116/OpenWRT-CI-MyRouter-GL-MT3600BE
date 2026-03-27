e59e1492b380819ccbd9cc652cb8cc660b5812f5
yjy116 <yjy116@gmail.com>
2026-03-27 16:36:14 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -3,21 +3,19 @@
 # 1. 修改默认 IP 为 192.168.18.1
 sed -i 's/192.168.1.1/192.168.18.1/g' package/base-files/files/bin/config_generate
 
-# 2. 修改默认主机名为 Beryl7-LEDE
-sed -i 's/OpenWrt/Beryl7-LEDE/g' package/base-files/files/bin/config_generate
+# 2. 修改默认主机名为 Beryl7-Immortal
+sed -i 's/OpenWrt/Beryl7-Immortal/g' package/base-files/files/bin/config_generate
 
 # 3. 设置默认主题为 Argon
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 
-# 4. 强制删除预设无线配置 (解决双频 WiFi 识别不全的关键)
-# 这一步会让系统在首次开机时根据驱动重新探测 radio0 和 radio1
+# 4. 强制删除无线配置 (确保开机重新探测 2.4G 和 5G)
 rm -f package/base-files/files/etc/config/wireless
 
-# 5. 开启硬件转发优化
+# 5. 设置 root 默认密码为 password (采用覆盖式注入，规避 sed 匹配失败)
+# 这一行直接创建或覆盖 shadow 文件，确保密码一定是 password
+mkdir -p package/base-files/files/etc/
+echo 'root:$1$V4UetPzk$CY6Sv6wSRCosSKqvePqgr0:18856:0:99999:7:::' > package/base-files/files/etc/shadow
+
+# 6. 开启 MTK WED 硬件转发加速
 echo "CONFIG_NET_MEDIATEK_SOC_WED=y" >> .config
-
-# 强制设置 root 密码为 password 使用lede默认密码 "password"
-# echo "password" | (passwd root <<EOF password password EOF)
-
-# 6. 修改版本号标识 (可选)
-sed -i "s/OpenWrt /Built by YourName $(date +%Y-%m-%d) /g" package/base-files/files/etc/banner
